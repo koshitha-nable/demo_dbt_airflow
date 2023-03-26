f25351623c1e9ddb56b5e1890a381936fd9f01ca
Koshitha Amarakoon <koshithaa@n-able.biz>
2023-03-26 23:54:33 +0530

    Analysis model updated

--- dbt_demo/yt_demo/models/analysis/analysis_yt_data.sql
+++ dbt_demo/yt_demo/models/analysis/analysis_yt_data.sql
@@ -1,8 +1,15 @@
 
 {{ config( materialized='table', table_name='analysis_yt_data' ) }}
-SELECT CHANNEL_NAME, SUBSCRIBER_COUNT
+
+SELECT
+    GENRE,
+    COUNT(ID) as total,
+    MAX(ID) as maximum,
+    MIN(ID) as minimum, 
+    AVG(ID) as average,
+    STDDEV(ID) as standardDeviation
+    
 FROM yt_data
-ORDER BY SUBSCRIBER_COUNT
-LIMIT 10
+GROUP BY GENRE
 
 
