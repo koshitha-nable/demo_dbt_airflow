edacf0f2d3e1efc7b3af09dd88196266a82d9ff7
koshitha-nable <113046544+koshitha-nable@users.noreply.github.com>
2023-03-30 10:13:58 +0530

    Delete dbt_demo directory

--- dbt_demo/dbt_project/models/transformation/aggregate_yt_data.sql
+++
@@ -1,7 +0,0 @@
-{{config(materialized = 'table', table_name = 'aggregate_yt_data')}}
-
-SELECT
-    GENRE,
-    COUNT(ID) as total
-FROM yt_data
-GROUP BY GENRE
--- dbt_demo/dbt_project/target/compiled/yt_demo/models/example/stg_yt_data.sql
+++
@@ -1,6 +0,0 @@
-
-
--- Load the data into the staged table
-SELECT *
-FROM DEMO.DEMO_03142023.yt_data
-;
